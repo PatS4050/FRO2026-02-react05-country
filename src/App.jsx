a164dcd0902f6c4b5c742bebaab1b920074c332f
PatS4050 <patrick@straatman.eu>
2026-05-12 18:58:26 +0200

    Opdracht 1 sub 5 gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css';
+import React, {setState} from "react";
+import axios from "axios";
+
 
 function App() {
+    // const [world, setWorld] = setState ()
+    const worldLink ='https://restcountries.com/v3.1/all?fields=name,flags,population,region';
+    // const worldLink ='https://restcountries.com/v3.1/all?fields=name';
+
+
+    async function getWorld () {
+        try {
+            const responseWorld = await axios.get(worldLink);
+            console.log(responseWorld.data[0].name);
+        } catch (e) {
+            console.error(e);
+        }
+
+
+    }
 
     return (
         <>
-            Maak je applicatie hier!
+           <h1>Maak je applicatie hier!</h1>
+            <button onClick={getWorld}>breng de wereld</button>
+            <p>resultaat</p>
         </>
     )
 }
