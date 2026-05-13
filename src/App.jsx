e24433e088d2e24ee516c5530607f4aaa790c8d7
PatS4050 <patrick@straatman.eu>
2026-05-13 11:57:18 +0200

    Opdracht 1 met 6 en 7 aan het stoeien

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import React, {setState} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 
 function App() {
-    // const [world, setWorld] = setState ()
+    const [countryName, setCountryName] = useState ("");
     const worldLink ='https://restcountries.com/v3.1/all?fields=name,flags,population,region';
     // const worldLink ='https://restcountries.com/v3.1/all?fields=name';
 
@@ -12,12 +12,20 @@ function App() {
     async function getWorld () {
         try {
             const responseWorld = await axios.get(worldLink);
-            console.log(responseWorld.data[0].name);
+            console.log(responseWorld.data);
         } catch (e) {
             console.error(e);
         }
-
-
+    }
+    async function getCountryName () {
+        try {
+            const responseCountryName = await axios.get(worldLink);
+            setCountryName (responseCountryName.data[0].name);
+            // return setCountryName = responseCountryName.data[0].name;
+            // console.log(responseCountryName.data[0].name);
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     return (
@@ -25,6 +33,10 @@ function App() {
            <h1>Maak je applicatie hier!</h1>
             <button onClick={getWorld}>breng de wereld</button>
             <p>resultaat</p>
+            <button onClick={getCountryName}>breng het eerste land</button>
+            <ul>
+                <li>naam land {countryName}</li>
+            </ul>
         </>
     )
 }
